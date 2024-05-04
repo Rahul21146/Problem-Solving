2801993fc6d03aca5e77e259db2b1447fab97286
Rahul Singh <124405795+Rahul21146@users.noreply.github.com>
2024-05-04 23:53:02 +0530

    Update 1155. Number of Dice Rolls With Target Sum.cpp

--- Homework/DP-2/1155. Number of Dice Rolls With Target Sum.cpp
+++ Homework/DP-2/1155. Number of Dice Rolls With Target Sum.cpp
@@ -29,6 +29,46 @@ Constraints:
 1 <= n, k <= 30
 1 <= target <= 1000
   */
+
+//memoisation
+
+class Solution {
+public:
+    int mod=10e9;
+    int solve(int n, int k, int target,vector<vector<int>> &dp){
+        //base case
+        if (target < 0) {
+            return 0;
+        }
+        if(target!=0 && n==0){
+            return 0;
+        }
+        if(n!=0 && target==0){
+            return 0;
+        }
+        
+        if(target==0 && n==0){
+            return 1;
+        }
+        if(dp[n][target]!=-1){
+           return dp[n][target]; 
+        }
+        int ans=0;
+        for(int i=1;i<=k;i++){
+            ans=(ans+solve(n-1,k,target-i,dp))%1000000007;;
+        }
+        dp[n][target]=ans;
+        return dp[n][target];
+    }
+    int numRollsToTarget(int n, int k, int target) {
+        vector<vector<int>> dp(n+1,vector<int>(target+1,-1));
+        return solve(n,k,target,dp);
+
+        
+    }
+};
+
+
 //bottom up apporach
 class Solution {
 public:
