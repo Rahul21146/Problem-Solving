feeb96f2d134ff2cc3cdaa4599415bd2f5af0179
Rahul Singh <124405795+Rahul21146@users.noreply.github.com>
2024-05-05 00:27:13 +0530

    Create 91. Decode Ways.cpp

---
+++ Homework/DP-2/91. Decode Ways.cpp
@@ -0,0 +1,71 @@
+/*
+A message containing letters from A-Z can be encoded into numbers using the following mapping:
+
+'A' -> "1"
+'B' -> "2"
+...
+'Z' -> "26"
+To decode an encoded message, all the digits must be grouped then mapped back into letters using the reverse of the mapping above (there may be multiple ways). For example, "11106" can be mapped into:
+
+"AAJF" with the grouping (1 1 10 6)
+"KJF" with the grouping (11 10 6)
+Note that the grouping (1 11 06) is invalid because "06" cannot be mapped into 'F' since "6" is different from "06".
+
+Given a string s containing only digits, return the number of ways to decode it.
+
+The test cases are generated so that the answer fits in a 32-bit integer.
+
+ 
+
+Example 1:
+
+Input: s = "12"
+Output: 2
+Explanation: "12" could be decoded as "AB" (1 2) or "L" (12).
+Example 2:
+
+Input: s = "226"
+Output: 3
+Explanation: "226" could be decoded as "BZ" (2 26), "VF" (22 6), or "BBF" (2 2 6).
+Example 3:
+
+Input: s = "06"
+Output: 0
+Explanation: "06" cannot be mapped to "F" because of the leading zero ("6" is different from "06").
+ 
+
+Constraints:
+
+1 <= s.length <= 100
+s contains only digits and may contain leading zero(s).
+*/
+
+//memoisation---------------------------------------->
+class Solution {
+public:
+    int solve(string s,int index,vector<int> &dp){
+        if(s[index]=='0'){
+            return 0;
+        }
+        if(index==s.size() || index==s.size()-1){
+            return 1;
+        }
+        
+        if(dp[index]!=-1){
+            return dp[index];
+        }
+        int ans=solve(s,index+1,dp);
+        
+        if(stoi(s.substr(index, 2)) <= 26){
+            ans=ans+solve(s,index+2,dp);
+        }
+        dp[index]=ans;
+        return dp[index];
+    }
+    int numDecodings(string s) {
+        int index=0;
+        int n=s.size();
+        vector<int> dp(n+1,-1);
+        return solve(s,index,dp);
+    }
+};
