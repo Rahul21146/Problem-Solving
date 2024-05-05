54f4f025fa114d8145c300f3d58202d64d106f2c
Rahul Singh <124405795+Rahul21146@users.noreply.github.com>
2024-05-05 15:12:53 +0530

    Update 322. Coin Change.cpp

--- Homework/DP-3/322. Coin Change.cpp
+++ Homework/DP-3/322. Coin Change.cpp
@@ -65,3 +65,26 @@ public:
          return ans == INT_MAX ?  -1 : ans;
     }
 };
+
+
+
+//bottom up apporach
+
+
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> dp(amount + 1, INT_MAX);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; ++i) {
+            for (int coin : coins) {
+                if (i - coin >= 0 && dp[i - coin] != INT_MAX) {
+                    dp[i] = std::min(dp[i], 1 + dp[i - coin]);
+                }
+            }
+        }
+        
+        return dp[amount] == INT_MAX ? -1 : dp[amount];
+    }
+};
+
