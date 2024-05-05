2b29e4c15dae397522fb20dbb966b4d09a01da3e
Rahul Singh <124405795+Rahul21146@users.noreply.github.com>
2024-05-05 15:57:24 +0530

    Update 518. Coin Change II.cpp

--- Homework/DP-3/518. Coin Change II.cpp
+++ Homework/DP-3/518. Coin Change II.cpp
@@ -86,3 +86,26 @@ public:
         return solve(amount,coins,index,dp);
     }
 };
+
+//botttom up apporach------------------------------------------>
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        int index=0;
+        int n=coins.size();
+        vector<vector<int>>dp(amount+1,vector<int>(n,-1));
+            for(int j=0;j<n;j++){
+                dp[0][j]=1;
+            }
+        
+        for(int i=1;i<=amount;i++){
+            for(int j=0;j<n;j++){
+                int include = (i - coins[j] >= 0) ? dp[i - coins[j]][j] : 0;
+                int exclude = (j >= 1) ? dp[i][j - 1] : 0;
+                dp[i][j] = include + exclude;
+            }
+        }
+        
+        return dp[amount][n-1];
+    }
+};
