5161d6e32de1fcbc71060da129697082dba6f1ea
Rahul Singh <124405795+Rahul21146@users.noreply.github.com>
2024-12-12 20:02:49 +0530

    Update Valid Path.cpp

--- Homework/Graph-2/Valid Path.cpp
+++ Homework/Graph-2/Valid Path.cpp
@@ -1,5 +1,6 @@
 /*
-There is a rectangle with the left top as (0, 0) and the right bottom as (x, y). There are N circles such that their centers are inside the rectangle. The radius of each circle is R. Now we need to find out if it is possible that we can move from (0, 0) to (x, y) without touching any circle.
+There is a rectangle with the left top as (0, 0) and the right bottom as (x, y). There are N circles such that their centers are inside the rectangle.
+The radius of each circle is R. Now we need to find out if it is possible that we can move from (0, 0) to (x, y) without touching any circle.
 
 Note :
 We can move from any cell to any of its 8 adjacent neighbors and we cannot move outside the boundary of the rectangle at any point of time.
